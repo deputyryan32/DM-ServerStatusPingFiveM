c931fff95e0114544564367bb1bbd7541bf64070
DeputyRyan <95866970+deputyryan32@users.noreply.github.com>
2025-01-14 20:10:18 -0500

    2.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,11 @@
 fx_version 'cerulean'
 game 'gta5'
 
-lua54 'yes' -- Enable Lua 5.4
+lua54 'yes'
 
 author 'DeputyRyan-DeputyMods'
-description 'DM-ServerStatusPingFiveM: Sends periodic pings to a status logger endpoint like BetterStack.'
-version '1.0.0'
+description 'DM-ServerStatusPingFiveM: Sends periodic pings to a status logger endpoint like BetterStack and Discord notifications.'
+version '2.0.0'
 
 server_scripts {
     'config.lua',
